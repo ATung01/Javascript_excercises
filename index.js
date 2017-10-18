59661eb3fa0d76306d7a3a315efa87d172e43d75
github username <acc.tung@gmail.com>
2017-10-18 17:58:33 -0400

    palindrome excercise

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 function testHello() {
   alert('Hello')
 }
-
-$(function() {
-
-
-});
---
+++ palindrome.js
@@ -0,0 +1,20 @@
+let letters = []
+
+let word = "racecar"
+
+let rword = ""
+
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i])
+}
+
+for (var i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+if (rword === word) {
+  console.log(word + " is a palindrome.")
+}
+else {
+  console.log(word + " is not a palindrome.")
+}
