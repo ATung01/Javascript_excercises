c9cbc2af8f40a521e3c26a41621323bdecaf525a
github username <acc.tung@gmail.com>
2017-10-25 18:50:45 -0400

    q2

--- rapidFireQuestions.js
+++ rapidFireQuestions.js
@@ -9,3 +9,16 @@ function question1() {
   }
 }
 // this shows "You clicked element 5" twice
+
+
+function handlerWrapper(i) {
+   return function() {
+      console.log('You clicked element #' + i);
+   }
+}
+function question2() {
+  var nodes = document.getElementsByTagName('button');
+  for (var i = 0; i < nodes.length; i++) {
+     nodes[i].addEventListener('click', handlerWrapper(i));
+  }
+}
