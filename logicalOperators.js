7a136f92be045067edbca5a49b5803ddba00c759
github username <acc.tung@gmail.com>
2017-11-22 16:45:45 -0500

    logical operator excercises

---
+++ logicalOperators.js
@@ -0,0 +1,8 @@
+console.log("0 || 1 = "+(0 || 1));
+console.log("1 || 2 = "+(1 || 2));
+console.log("1 || 3 = "+(1 || 3));
+console.log("2 || 3 = "+(2 || 3));
+console.log("0 && 1 = "+(0 && 1));
+console.log("1 && 2 = "+(1 && 2));
+console.log("1 && 3 = "+(1 && 3));
+console.log("2 && 3 = "+(2 && 3));
