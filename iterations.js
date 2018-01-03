fa59a2bfff8c4c66d382b6de40da0570f7ec3caf
github username <acc.tung@gmail.com>
2018-01-03 16:07:11 -0500

    starting codility demo lessons, first up iterations

---
+++ iterations.js
@@ -0,0 +1,5 @@
+/* Given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+
+*/
