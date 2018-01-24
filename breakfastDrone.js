e0141481083fd34abf031e4c26c08a3fffbdf9a3
github username <acc.tung@gmail.com>
2018-01-23 23:28:45 -0500

    not confident in bitwise operators yet, refactor into something I'm more familiar with

--- breakfastDrone.js
+++ breakfastDrone.js
@@ -1,14 +1,33 @@
 
 let testArray = [1, 2, 3, 2, 1]
 
+// function findUniqueDeliveryId(deliveryIds) {
+//
+//   var uniqueDeliveryId = 0;
+//
+//   deliveryIds.forEach(function(deliveryId) {
+//     console.log(uniqueDeliveryId.toString(2), deliveryId.toString(2))
+//       uniqueDeliveryId ^= deliveryId;
+//   });
+//
+//   return uniqueDeliveryId;
+// }
+
+
 function findUniqueDeliveryId(deliveryIds) {
 
-  var uniqueDeliveryId = 0;
+  let deliveries= {}
 
-  deliveryIds.forEach(function(deliveryId) {
-    console.log(uniqueDeliveryId.toString(2), deliveryId.toString(2))
-      uniqueDeliveryId ^= deliveryId;
-  });
+  for (let i = 0; i < deliveryIds.length; i++) {
+    deliveries[deliveryIds[i]] = 1 + (deliveries[deliveryIds[i]] || 0)
+  }
 
-  return uniqueDeliveryId;
+  for (let x in deliveries) {
+
+    if (deliveries[x] === 1) {
+      return x;
+    }
+  }
+
+  return "didn't find it"
 }
