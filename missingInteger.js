51626843afc0ea343d624b168f3d01f3a8a8a56b
github username <acc.tung@gmail.com>
2018-01-31 04:16:41 -0500

    similiar excercise to previous codility problem, 100%

---
+++ missingInteger.js
@@ -0,0 +1,36 @@
+/*
+Write a function:
+
+function solution(A);
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Assume that:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+*/
+
+function solution(A) {
+    let numCounter = {}
+
+    for(let i = 0; i < A.length; i++){
+        numCounter[A[i]] = 1;
+    }
+    for(let j = 1; j <= A.length; j++){
+        if (numCounter[j] !== 1) {
+            return j;
+        }
+    }
+    return A.length + 1
+}
