2fbc91ab3fef0246c395bf4c9adbb9365186ad89
github username <acc.tung@gmail.com>
2018-03-07 18:48:17 -0500

    object created storing every word and its count

--- attentive.js
+++ attentive.js
@@ -1,5 +1,18 @@
 // Given a string and K, find the top K most common words in the string
 
-function findTop() {
+function findTop(string, k) {
+  string = string.toLowerCase().split(" ")
+
+  let wordStore = {}
+
+  for (let i = 0; i < string.length; i++) {
+    wordStore[string[i]] = 1 + (wordStore[string[i]] || 0)
+  }
+  console.log(wordStore)
 
 }
+
+let exampleString = "This is an example this sort of question really needs and this and this and this too"
+
+
+findTop(exampleString, 3)
