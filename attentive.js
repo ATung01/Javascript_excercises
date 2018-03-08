85247e2bcf44dcc7c993defb69221bb4256e9a73
github username <acc.tung@gmail.com>
2018-03-07 19:02:12 -0500

    solution

--- attentive.js
+++ attentive.js
@@ -8,8 +8,12 @@ function findTop(string, k) {
   for (let i = 0; i < string.length; i++) {
     wordStore[string[i]] = 1 + (wordStore[string[i]] || 0)
   }
-  console.log(wordStore)
-
+  // console.log(wordStore)
+  let sortedArr = Object.keys(wordStore).sort(
+    (a, b) => wordStore[b] - wordStore[a]
+  )
+  console.log(sortedArr.slice(0, k))
+  return sortedArr.slice(0, k)
 }
 
 let exampleString = "This is an example this sort of question really needs and this and this and this too"
