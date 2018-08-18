6d74bfb288f2852c8114bc7d8a5f58e551033a2c
Atung01 <Acc.tung@gmail.com>
2018-08-18 12:44:05 -0500

    fixing pathspec error?

---
+++ mostFreq.js
@@ -0,0 +1,4 @@
+// "Given the set a a b b a a c c b b of unknown length, write an algorithm that figures out which occurs most frequently and with the most continuous repetition."
+
+
+const TEST = ['a', 'a', 'b', 'b', 'a', 'a', 'c', 'c', 'b', 'b' ]
